544d6e37eeeb70cc82e3cf90e76f5e924dc785b9
Alphraneous <bimandivcreations@gmail.com>
2021-05-30 17:51:00 -0500

    Fixed bugs

--- NightShade_AMZ/src/NightShade_AMZ/gpuBot.java
+++ NightShade_AMZ/src/NightShade_AMZ/gpuBot.java
@@ -6,15 +6,17 @@ import java.io.FileNotFoundException;
 import java.net.*;
 import java.util.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.util.Random;
 
 
 public class gpuBot {
 	
     public static void main(String[] args) {
+    	
 /* 
  _   _ _       _     _   _____ _               _       
 | \ | (_)     | |   | | /  ___| |             | |      
@@ -37,7 +39,21 @@ Please message Ajax21#5396 on discord for issues
 
         System.out.println("Starting");
         //Please set path to chromedriver here\
-        File config = new File("config.txt");
+        File config = new File("credentials.txt");
+        Boolean configFileFound = true;
+        String username = null;
+        String password = null;
+        try {
+			Scanner configReader = new Scanner(config);
+			username = configReader.nextLine();
+			password = configReader.nextLine();
+			System.out.println("Config file found");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Credentials file not found, continuing");
+			configFileFound = false;
+		}
+        
         System.setProperty("webdriver.chrome.driver","lib\\chromedriver.exe");
         //Hello World, Starting Webdriver
         WebDriver wd = new ChromeDriver();
@@ -51,18 +67,37 @@ Please message Ajax21#5396 on discord for issues
         int cutoffPrice = Integer.parseInt(priceCut.nextLine()); 
         //Open the amazon sign in page so the user can sign in
         wd.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Faws%2Fcart%2Fadd.html%3F%26ref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
-        System.out.println("Please Sign In And Press Any Key to Start Bot");
-        Scanner userInput = new Scanner(System.in);
-        String inputNull = userInput.nextLine(); 
+        if (configFileFound) {
+        	WebElement emailBox = wd.findElement(By.xpath("//*[@id='ap_email']"));
+        	emailBox.sendKeys(username);
+        	WebElement continueButton = wd.findElement(By.xpath("//*[@id=\'continue\']"));
+        	continueButton.click();
+        	WebElement passwordBox = wd.findElement(By.xpath("//*[@id=\'ap_password\']"));
+        	passwordBox.sendKeys(password);
+        	WebElement loginButton = wd.findElement(By.xpath("//*[@id=\'signInSubmit\']"));
+        	loginButton.click();
+        } else {
+        	System.out.println("Please Sign In And Press Any Key to Start Bot");
+        	Scanner userInput = new Scanner(System.in);
+            String inputNull = userInput.nextLine(); 
+        }
         // NOTE: userInput is not used because it is simply to stop the below code from running until the user has logged in
         System.out.println("Bot Started, Best of Luck!");
         wd.get(productURL);
         while (true) {
         //See if the product is in stock by seeing if a price textbox is visible
-        boolean isInStock = wd.findElements(By.xpath("//*[@id=\'availability\']/span")).size() == 0;
-        if (isInStock) {
+        try {
+        boolean isOutOfStock = wd.findElements(By.xpath("//*[@id=\'availability\']/span")).size() > 0;
+        Random delay = new Random();
+        double delayAmount = 0.5 + (1.5 - 0.5) * delay.nextDouble();
+        if (!isOutOfStock) {
             //Retrieve Price
-            String price = wd.findElement(By.xpath("//*[@id='price_inside_buybox']")).getText();
+        	String price = null;
+			if (wd.findElements(By.xpath("//*[@id='price_inside_buybox']")).size() > 0) {
+        		price = wd.findElement(By.xpath("//*[@id='price_inside_buybox']")).getText();
+        	} else if (wd.findElements(By.xpath("//*[@id=\'priceblock_ourprice\']")).size() > 0) {
+        		price = wd.findElement(By.xpath("//*[@id=\'priceblock_ourprice\']")).getText();
+        	}
             //Format the text to be readable as an integer
             String priceRev1 = price.substring(1,(price.length()-3));
             String priceRev2 = priceRev1.replaceAll(",","");
@@ -73,7 +108,7 @@ Please message Ajax21#5396 on discord for issues
                 //If the price of the item is too high, refresh the page
                 System.out.println("Price is too high, retrying");
                 try {
-					TimeUnit.SECONDS.sleep((long) 0.5);
+					TimeUnit.SECONDS.sleep((long) delayAmount);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -81,12 +116,18 @@ Please message Ajax21#5396 on discord for issues
                 wd.navigate().refresh();
             } else {
             //If the product is in stock for under the user specified price, proceed with the order
-            WebElement buyNowButton = wd.findElement(By.xpath("//*[@id='buy-now-button']"));
+            WebElement buyNowButton = wd.findElement(By.xpath("//*[@id=\'buy-now-button\']"));
             buyNowButton.click();
             while (true) {
-                boolean placeOrderVisible = wd.findElements(By.xpath("//*[@id=\'placeYourOrder\']/span/input")).size() > 0;
-                	if (placeOrderVisible) {
+                boolean placeOrder1Visible = wd.findElements(By.xpath("//*[@id=\'placeYourOrder\']/span/input")).size() > 0;
+                boolean placeOrder2Visible = wd.findElements(By.xpath("//*[@id=\'turbo-checkout-pyo-button\']")).size() > 0;
+                	if (placeOrder1Visible) {
                 		WebElement placeOrderButton = wd.findElement(By.xpath("//*[@id=\'placeYourOrder\']/span/input"));
+                    	placeOrderButton.click();
+                    	System.out.println("Product Successfully Purchased, Browser Will Close in 5 Seconds");
+                    	break;
+                	} else if (placeOrder2Visible) {
+                		WebElement placeOrderButton = wd.findElement(By.xpath("//*[@id=\'turbo-checkout-pyo-button\']/span/input"));
                     	placeOrderButton.click();
                     	System.out.println("Product Successfully Purchased, Browser Will Close in 5 Seconds");
                     	break;
@@ -108,12 +149,18 @@ Please message Ajax21#5396 on discord for issues
             //If the item isn't in stock, refresh the page
             System.out.println(wd.findElement(By.xpath("//*[@id=\'availability\']/span")).getText());
             try {
-				TimeUnit.SECONDS.sleep((long) 0.5);
+				TimeUnit.SECONDS.sleep((long) delayAmount);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
             wd.navigate().refresh();
+        }
+        } catch (NoSuchWindowException e) {
+        	System.out.println("Window Closed");
+        	System.out.println(e);
+        	wd.quit();
+        	break;
         }
         }
     }  
